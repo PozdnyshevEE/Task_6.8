49105da695e7cf44411cf5a581e325719e8ddfef
Евгений Позднышев <pozdnyshevEE@yandex.ru>
2023-02-06 17:38:38 +0300

    task5 modify

--- task5/script.js
+++ task5/script.js
@@ -3,13 +3,12 @@ const duplicateField = document.querySelector('#duplicateField');
 const consoleButton = document.querySelector('#consoleButton');
 
 inputText.addEventListener('input', (event) => {
-    
     duplicateField.textContent = event.target.value;
 })
 
 consoleButton.addEventListener('click', (event) => {
-    let textConsole = event.target.value;
-    console.log(textConsole);
+    event.preventDefault();
+    console.log(inputText.value);
     inputText.value = '';
     duplicateField.textContent = '';
 })
