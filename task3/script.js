b3eb1624d92547f1461094547f13932ae47460ab
Евгений Позднышев <pozdnyshevEE@yandex.ru>
2023-02-08 14:31:45 +0300

    final_commit

--- task3/script.js
+++ task3/script.js
@@ -1,16 +1,25 @@
+// Объявляем переменную с постоянным значением.
+
 const trafficLightGreen = document.querySelector('#trafficLight');
 const trafficLightYellow = document.querySelector('#trafficLight1');
 const trafficLightRed = document.querySelector('#trafficLight2');
 
 trafficLightRed.addEventListener('click', makeRed);
 function makeRed() {
+    
+    // Меняем цвет Div-ов
+
     trafficLightRed.style.background = ('red');
     trafficLightYellow.style.background = ('black');
     trafficLightGreen.style.background = ('black');
 
+    // Удаляем обработчик события, который был ранее зарегистрирован
+
     trafficLightGreen.removeEventListener('click', makeRed);
     trafficLightYellow.removeEventListener('click', makeRed);
     trafficLightRed.removeEventListener('click', makeRed);
+
+    // Регистрируем обработчик события
 
     trafficLightGreen.addEventListener('click', makeGreen);
     trafficLightYellow.addEventListener('click', makeGreen);
@@ -19,13 +28,20 @@ function makeRed() {
 
 trafficLightYellow.addEventListener('click', makeYellow);
 function makeYellow() {
+
+    // Меняем цвет Div-ов
+
     trafficLightYellow.style.background = ('yellow');
     trafficLightGreen.style.background = ('black');
     trafficLightRed.style.background = ('black');
 
+    // Удаляем обработчик события, который был ранее зарегистрирован
+
     trafficLightGreen.removeEventListener('click', makeYellow);
     trafficLightYellow.removeEventListener('click', makeYellow);
     trafficLightRed.removeEventListener('click', makeYellow);
+
+    // Регистрируем обработчик события
 
     trafficLightGreen.addEventListener('click', makeRed);
     trafficLightYellow.addEventListener('click', makeRed);
@@ -34,13 +50,20 @@ function makeYellow() {
 
 trafficLightGreen.addEventListener('click', makeGreen);
 function makeGreen() {
+
+    // Меняем цвет Div-ов
+
     trafficLightGreen.style.background = ('green');
     trafficLightRed.style.background = ('black');
     trafficLightYellow.style.background = ('black');
 
+    // Удаляем обработчик события, который был ранее зарегистрирован
+
     trafficLightGreen.removeEventListener('click', makeGreen);
     trafficLightYellow.removeEventListener('click', makeGreen);
     trafficLightRed.removeEventListener('click', makeGreen);
+
+    // Регистрируем обработчик события
 
     trafficLightGreen.addEventListener('click', makeYellow);
     trafficLightYellow.addEventListener('click', makeYellow);
